f4af5fd0cee9a82ded0b3836cbd9a4d724177046
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-16 17:58:41 -0300

    feat: add routes to BarberShop

--- src/modules/barberShop/barberShop.routes.ts
+++ src/modules/barberShop/barberShop.routes.ts
@@ -4,11 +4,7 @@ import { BarberShopController } from "./barberShop.controller";
 const router = Router();
 const barberShopController = new BarberShopController(); // Instantiate the controller
 
-// Define routes for BarberShop
 router.get("/recent", barberShopController.getRecentlyAdded);
-
-// Example for other routes you might add:
-// router.get('/:id', barberShopController.getBarberShopById);
-// router.post('/', barberShopController.createBarberShop);
+router.post("/", barberShopController.createNewBarberShop);
 
 export default router;
