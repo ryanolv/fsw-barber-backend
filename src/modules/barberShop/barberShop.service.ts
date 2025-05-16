b918275f3c315b6781e4cb410d0955517a1e4424
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-16 17:57:27 -0300

    feat: add BarberShopService

--- src/modules/barberShop/barberShop.service.ts
+++ src/modules/barberShop/barberShop.service.ts
@@ -1,21 +1,20 @@
 import { BarberShopRepository } from "./barberShop.repository";
-import { IBarberShopService, IBarberShopRepository, BarberShopDTO } from "./barberShop.types";
+import { IBarberShopService, IBarberShopRepository, BarberShopDTO, CreateBarberShopDTO } from "./barberShop.types";
 
 export class BarberShopService implements IBarberShopService {
   private barberShopRepository: IBarberShopRepository;
 
-  // Dependency Injection: Inject the repository through the constructor
   constructor(barberShopRepository: IBarberShopRepository = new BarberShopRepository()) {
     this.barberShopRepository = barberShopRepository;
   }
 
   async getRecentlyAddedBarberShops(): Promise<BarberShopDTO[]> {
-    // The business logic is simple here: fetch 5 recent barbershops.
-    // More complex logic (e.g., checking user permissions, combining data)
-    // would go in this service layer.
     const limit = 5;
     return this.barberShopRepository.findRecentlyAdded(limit);
   }
 
-  // Implement other IBarberShopService methods here as needed
+  async createNewBarberShop(data: CreateBarberShopDTO): Promise<BarberShopDTO> {
+    const newBarberShop = await this.barberShopRepository.create(data);
+    return newBarberShop;
+  }
 }
