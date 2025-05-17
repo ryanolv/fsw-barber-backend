de6a70bcb677b8d169fa36c15e1ad1094dc84ff7
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-17 04:58:36 -0300

    feat: add getBarberShopById and searchBarberShops methods

--- src/modules/barberShop/barberShop.controller.ts
+++ src/modules/barberShop/barberShop.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { BarberShopService } from "./barberShop.service";
 import { IBarberShopService } from "./barberShop.types";
-import { createBarberShopSchema, CreateBarberShopInput } from "./barberShop.validation";
+import {
+  createBarberShopSchema,
+  CreateBarberShopInput,
+  getBarberShopParamsSchema,
+  searchBarberShopsQuerySchema,
+} from "./barberShop.validation";
 
 export class BarberShopController {
   private barberShopService: IBarberShopService;
@@ -14,6 +19,25 @@ export class BarberShopController {
     try {
       const barberShops = await this.barberShopService.getRecentlyAddedBarberShops();
       res.status(200).json(barberShops);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getBarberShopById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const paramsValidationResult = await getBarberShopParamsSchema.safeParseAsync(req);
+
+      if (!paramsValidationResult.success) {
+        res.status(400).json({ errors: paramsValidationResult.error.format() });
+        return;
+      }
+
+      const { barberShopId } = paramsValidationResult.data.params;
+
+      const barberShop = await this.barberShopService.getBarberShopByIdWithServices(barberShopId);
+
+      res.status(200).json(barberShop);
     } catch (error) {
       next(error);
     }
@@ -32,6 +56,23 @@ export class BarberShopController {
       const newBarberShop = await this.barberShopService.createNewBarberShop(validatedData);
 
       res.status(201).json(newBarberShop);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public searchBarberShops = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const queryValidationResult = await searchBarberShopsQuerySchema.safeParseAsync(req);
+
+      if (!queryValidationResult.success) {
+        res.status(400).json({ errors: queryValidationResult.error.format() });
+        return;
+      }
+
+      const { q } = queryValidationResult.data.query;
+      const barberShops = await this.barberShopService.searchBarberShopsByTitle(q);
+      res.status(200).json(barberShops);
     } catch (error) {
       next(error);
     }
