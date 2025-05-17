53e12b7b9c28ff120faad8d0dc4db20ab5d75676
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-17 03:21:07 -0300

    feat: increment services route to barbershop routes

--- src/modules/barberShop/barberShop.routes.ts
+++ src/modules/barberShop/barberShop.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { BarberShopController } from "./barberShop.controller";
+import serviceRoutes from "../service/service.routes";
 
 const router = Router();
-const barberShopController = new BarberShopController(); // Instantiate the controller
+const barberShopController = new BarberShopController();
 
 router.get("/recent", barberShopController.getRecentlyAdded);
 router.post("/", barberShopController.createNewBarberShop);
+
+router.use("/:barberShopId/services", serviceRoutes);
 
 export default router;
