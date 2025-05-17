691e75ab6b7af51501bd30ae88278c5a22545152
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-17 03:23:01 -0300

    feat: add findById method into BarberShopRepository

--- src/modules/barberShop/barberShop.repository.ts
+++ src/modules/barberShop/barberShop.repository.ts
@@ -12,6 +12,13 @@ export class BarberShopRepository implements IBarberShopRepository {
     return barberShops;
   }
 
+  async findById(id: string): Promise<BarberShopDTO | null> {
+    const barberShop = await prisma.barberShop.findUnique({
+      where: { id },
+    });
+    return barberShop;
+  }
+
   async create(data: CreateBarberShopDTO): Promise<BarberShopDTO> {
     const newBarberShop = await prisma.barberShop.create({
       data: {
--- src/modules/barberShop/barberShop.types.ts
+++ src/modules/barberShop/barberShop.types.ts
@@ -7,6 +7,7 @@ export type CreateBarberShopDTO = CreateBarberShopInput;
 export interface IBarberShopRepository {
   findRecentlyAdded(limit: number): Promise<BarberShopDTO[]>;
   create(data: CreateBarberShopDTO): Promise<BarberShopDTO>;
+  findById(id: string): Promise<BarberShopDTO | null>;
 }
 
 export interface IBarberShopService {
