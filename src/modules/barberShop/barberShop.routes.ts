04f3f043f8b209980da3c79a45bac600b3b954ec
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-17 04:59:52 -0300

    feat: add "/search" and "/:barberShopId" routes

--- src/modules/barberShop/barberShop.routes.ts
+++ src/modules/barberShop/barberShop.routes.ts
@@ -6,6 +6,10 @@ const router = Router();
 const barberShopController = new BarberShopController();
 
 router.get("/recent", barberShopController.getRecentlyAdded);
+router.get("/search", barberShopController.searchBarberShops);
+
+router.get("/:barberShopId", barberShopController.getBarberShopById);
+
 router.post("/", barberShopController.createNewBarberShop);
 
 router.use("/:barberShopId/services", serviceRoutes);
