df1fc912a2c0581e294778e9a4dc0f911b770777
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-17 04:56:13 -0300

    feat: add findbyIdWithServices and searchByTitle methods to interface

--- src/modules/barberShop/barberShop.types.ts
+++ src/modules/barberShop/barberShop.types.ts
@@ -1,16 +1,22 @@
-import { BarberShop } from "@prisma/client";
+import { BarberShop, Service } from "@prisma/client";
 import { CreateBarberShopInput } from "./barberShop.validation";
 
+export type BarberShopWithServicesDTO = BarberShop & { services?: Service[] };
 export type BarberShopDTO = BarberShop;
+
 export type CreateBarberShopDTO = CreateBarberShopInput;
 
 export interface IBarberShopRepository {
   findRecentlyAdded(limit: number): Promise<BarberShopDTO[]>;
   create(data: CreateBarberShopDTO): Promise<BarberShopDTO>;
   findById(id: string): Promise<BarberShopDTO | null>;
+  findByIdWithServices(id: string): Promise<BarberShopWithServicesDTO | null>;
+  searchByTitle(searchTerm: string): Promise<BarberShopDTO[]>;
 }
 
 export interface IBarberShopService {
   getRecentlyAddedBarberShops(): Promise<BarberShopDTO[]>;
   createNewBarberShop(data: CreateBarberShopDTO): Promise<BarberShopDTO>;
+  getBarberShopByIdWithServices(id: string): Promise<BarberShopWithServicesDTO | null>;
+  searchBarberShopsByTitle(searchTerm: string): Promise<BarberShopDTO[]>;
 }
